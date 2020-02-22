d6db00f7d19217eb205c5516d0353f1b7809485c
jarokaz <jarokaz@no-reply.com>
2020-02-22 19:16:15 +0000

    Updates

--- run_on_caip.sh
+++ run_on_caip.sh
@@ -17,29 +17,32 @@ TRAINING_DATASET=gs://workshop-datasets/covertype/evaluation/dataset.csv
 SCORING_MEASURE=accuracy
 SEARCH_SPACE="[\
 {'classifier':'sklearn.linear_model.SGDClassifier',\
-'classifier__alpha':[0.0002,0.0004,0.0008,0.001],\
-'classifier__max_iter':[500,1000]},\
+'classifier__alpha':[0.0001,0.0002,0.0004,0.0008,0.001],\
+'classifier__max_iter':[250,500,1000]},\
 {'classifier':'sklearn.neighbors.KNeighborsClassifier',\
-'classifier__n_neighbors':[5,7],\
+'classifier__n_neighbors':[3,5,7,10],\
 'classifier__p':[1,2]}\
 ]"
 
 
-python hypertune.py $TRAINING_DATASET $SEARCH_SPACE $SCORING_MEASURE 
+IMAGE_URI=gcr.io/mlops-workshop/dask_ml_traininer
+JOB_NAME=JOB_$(date +"%Y%m%d%s")
+JOB_DIR=gs://mlops-workshop-artifact-store/jobs/$JOB_NAME
+CPU_NUMBER=4
+MASTER_MACHINE_TYPE=n1-standard-${CPU_NUMBER}
+REGION=us-central1
 
-#{'classifier':'sklearn.svm.SVC',\
-#'classifier__kernel':['linear'],\
-#'classifier__C':[0.025]}\
+gcloud ai-platform jobs submit training $JOB_NAME \
+--region=$REGION \
+--job-dir=$JOB_DIR \
+--master-image-uri=$IMAGE_URI \
+--scale-tier=CUSTOM \
+--master-machine-type=$MASTER_MACHINE_TYPE \
+-- \
+--training_dataset_path=$TRAINING_DATASET \
+--search_space=$SEARCH_SPACE \
+--scoring_measure=$SCORING_MEASURE
 
-#{'classifier':'sklearn.neural_network.MLPClassifier',\
-#'classifier__alpha':[0.0001,0.0004],\
-#'classifier__max_iter':[200]}\
 
-#{'classifier':'sklearn.neighbors.KNeighborsClassifier',\
-#'classifier__n_neighbors':[5],\
-#'classifier__p':[2]}\
 
-#{'classifier':'sklearn.linear_model.SGDClassifier',\
-#'classifier__alpha':[0.0002,0.0004,0.0008,0.001],\
-#'classifier__max_iter':[500,1000]},\
 
