066bbfab58cc8cb5d81a658f84a1aa8e404716e8
jarokaz <jarokaz@no-reply.com>
2020-02-24 20:26:53 +0000

    Updates

--- run_on_caip.sh
+++ run_on_caip.sh
@@ -17,10 +17,10 @@ TRAINING_DATASET=gs://workshop-datasets/covertype/evaluation/dataset.csv
 SCORING_MEASURE=accuracy
 SEARCH_SPACE="[\
 {'classifier':'sklearn.linear_model.SGDClassifier',\
-'classifier__alpha':[0.0001,0.0002,0.0004,0.0008,0.001],\
-'classifier__max_iter':[250,500,1000]},\
+'classifier__alpha':[0.0001,0.0002,0.0004,0.0008,0.001,0.002],\
+'classifier__max_iter':[250,500,750,1000]},\
 {'classifier':'sklearn.neighbors.KNeighborsClassifier',\
-'classifier__n_neighbors':[3,5,7,10],\
+'classifier__n_neighbors':[3,5,7,10,12,14],\
 'classifier__p':[1,2]}\
 ]"
 
@@ -32,10 +32,10 @@ JOB_NAME=JOB_$(date +"%Y%m%d%s")
 JOB_DIR=${JOB_BUCKET}/jobs/${JOB_NAME}
 REGION=us-central1
 
-CPU_NUMBER=16
+CPU_NUMBER=64
 MASTER_MACHINE_TYPE=n1-highmem-${CPU_NUMBER}
 THREADS_PER_WORKER=4
-N_WORKERS=4
+N_WORKERS=16
 
 
 gcloud ai-platform jobs submit training $JOB_NAME \
@@ -47,9 +47,9 @@ gcloud ai-platform jobs submit training $JOB_NAME \
 -- \
 --training_dataset_path=$TRAINING_DATASET \
 --search_space=$SEARCH_SPACE \
---scoring_measure=$SCORING_MEASURE \
---n_workers=$N_WORKERS \
---threads_per_worker=$THREADS_PER_WORKER
+--scoring_measure=$SCORING_MEASURE 
+#--n_workers=$N_WORKERS \
+#--threads_per_worker=$THREADS_PER_WORKER
 
 
 
